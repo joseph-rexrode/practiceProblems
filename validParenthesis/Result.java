21ae75bb957906a53eb6bc67ec37a59ac33a22a7
jrexrode <jrexrode@me.com>
2023-01-17 13:16:53 -0500

    validParenthesis problem

---
+++ validParenthesis/Result.java
@@ -0,0 +1,60 @@
+package validParenthesis;
+
+import java.util.ArrayList;
+
+public class Result {
+    public boolean isValid(String s) {
+        // if s[i-1] is open bracket, s[i] must either be:
+            // close bracket same type
+            // open bracket
+        // if s[i-1] is close bracket, s[i] must either be:
+            // open bracket
+            // another close bracket 
+
+        ArrayList<Character> open = new ArrayList<Character>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(' ||
+                s.charAt(i) == '[' ||
+                s.charAt(i) == '{') {
+                    open.add(open.size(), s.charAt(i));
+                }
+            else {
+                switch(s.charAt(i)) {
+                    case ')':
+                        if ((open.size() == 0) || 
+                            open.get(open.size() - 1) != '(') {
+                                return false;
+                        }
+                        else {
+                            System.out.println("This is fine");
+                            open.remove(open.size() - 1);
+                            break;
+                        }
+                    case ']':
+                        if ((open.size() == 0) || 
+                            open.get(open.size() - 1) != '[') {
+                                return false;
+                        }
+                        else {
+                            System.out.println("This is fine");
+                            open.remove(open.size() - 1);
+                            break;
+                        }
+                    case '}':
+                        if ((open.size() == 0) || 
+                            open.get(open.size() - 1) != '{') {
+                                return false;
+                        }
+                        else {
+                            System.out.println("This is fine");
+                            open.remove(open.size() - 1);
+                            break;
+                        }
+                }
+            }
+        }
+        if (open.size() > 0) { return false; }
+        return true;
+    }
+}
---
+++ validParenthesis/Test.java
@@ -0,0 +1,8 @@
+package validParenthesis;
+
+public class Test {
+    public static void main(String[] args) {
+        Result result = new Result();
+        System.out.println(result.isValid("{{{"));
+    }
+}
