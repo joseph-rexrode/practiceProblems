864cc616d383d63b388d5b8c41d5c36a803c806c
jrexrode <jrexrode@me.com>
2023-01-17 16:19:20 -0500

    mergeSortedLists problem

---
+++ mergeSortedLists/Result.java
@@ -0,0 +1,52 @@
+package mergeSortedLists;
+
+public class Result {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+
+        if (list1 == null) { return list2; }
+        else if (list2 == null) { return list1; }
+        else {
+            ListNode solution = new ListNode();
+            if (list1.val <= list2.val) {
+                solution.val = list1.val;
+                list1 = list1.next;
+            }
+            else {
+                solution.val = list2.val;
+                list2 = list2.next;
+            }
+
+            ListNode it = solution;
+
+            while (list1 != null || list2 != null) {
+                // if no more values in list1
+                // or list2 value < list1 value --> add list2 value
+                if (list1 == null) {
+                    it.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                    it = it.next;
+                }
+
+                else if (list2 == null) {
+                    it.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                    it = it.next;
+                }
+
+                else if (list2.val < list1.val) {
+                    it.next = new ListNode(list2.val);
+                    list2 = list2.next;
+                    it = it.next;
+                }
+
+                else {
+                    it.next = new ListNode(list1.val);
+                    list1 = list1.next;
+                    it = it.next;
+                }
+            }
+            return solution;
+        }
+    }
+}
---
+++ mergeSortedLists/Test.java
@@ -0,0 +1,10 @@
+package mergeSortedLists;
+
+public class Test {
+    public static void main(String[] args) {
+        Result result = new Result();
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
+        System.out.println(result.mergeTwoLists(node1, node2));
+    }
+}
