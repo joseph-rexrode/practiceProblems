e6271c54ca7c4bd7fcc04a9e34086fc90f2582a7
jrexrode <jrexrode@me.com>
2023-01-25 07:29:18 -0500

    climbingStairs problem - fibonacci

---
+++ javaCode/climbingStairs/ClimbStairsTest.java
@@ -0,0 +1,8 @@
+package javaCode.climbingStairs;
+
+public class ClimbStairsTest {
+    public static void main(String[] args) {
+        ClimbStairs climb = new ClimbStairs();
+        System.out.println(climb.climbStairs(45));
+    }
+}
