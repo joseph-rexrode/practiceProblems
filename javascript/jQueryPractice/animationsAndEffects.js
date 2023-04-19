9da818103500a6f7b35b2653568ead4966436ea6
jrexrode <jrexrode@me.com>
2023-04-19 07:37:15 -0400

    added to animations and effects jQuery study

--- javascript/jQueryPractice/animationsAndEffects.js
+++ javascript/jQueryPractice/animationsAndEffects.js
@@ -10,3 +10,57 @@ $(document).ready(function() {
         })
     })
 });
+
+
+// fade effect
+
+$(document).ready(function() {
+    $('.item-box').on('click', 'a.info-link', function(event) {
+        event.preventDefault();
+
+        // fadeIn(), fadeOut(), fadeToggle() - makes content inside item-box fade, not box itself
+        $(this).closest('.item-box').find('more-info').fadeToggle();
+    })
+});
+
+
+// slide effect 
+
+$(document).ready(function() {
+    $('.item-box').on('click', '.info-link', function(event) {
+        event.preventDefault();
+
+        // slideDown(), slideUp(), slideToggle()
+        $(this).closest('.item-box').find('more-info').slideToggle('fast');
+    })
+});
+
+
+// animations for CSS properties
+
+    // animate method can ONLY change numerical properties, so no color
+
+$(document).ready(function() {
+    $('#example1').on('click', function() {
+        $(this).animate({
+            opacity: '-=0.3' // decreases by a set amount each time
+            }, 500, function() {
+            alert('completed!');
+        })
+    })
+
+    $('#example2').on('click', function() {
+        $(this).animate({
+            left: 200,
+            width: 40
+        }, 500)
+    })
+
+    $('#example3').on('click', function() {
+        $(this).animate({
+            left: '+=20',
+            width: '+=50'
+        }, 500)
+    })
+});
+
