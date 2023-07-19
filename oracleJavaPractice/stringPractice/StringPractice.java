46f3e0a59ab30a653e408c2684d47e256970c446
jrexrode <jrexrode@me.com>
2023-07-19 17:58:10 -0500

    added examples of StringBuilder class

--- oracleJavaPractice/stringPractice/StringPractice.java
+++ oracleJavaPractice/stringPractice/StringPractice.java
@@ -11,5 +11,18 @@ public class StringPractice {
         b = b.replace('B', '2').replace('C', '3');
 
         System.out.println("a: " + a + "\nb: " + b);
+
+
+        // STRING BUILDER CLASS
+
+        StringBuilder alpha = new StringBuilder();
+
+        // prevents inefficiency of using immutable String class
+
+        for (char current = 'a'; current <= 'z'; current++) {
+            alpha.append(current);
+        }
+
+        System.out.println(alpha);
     }
 }
